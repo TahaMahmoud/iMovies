58ccd0b0e28751b9dc996a687eede8582b4a55ca
Taha Mahmoud <taha.nagy06@gmail.com>
2024-01-19 11:52:41 +0200

    Add rating view and empty state

---
+++ iMovies/DesignSystem/Sources/DesignSystem/Components/Product/EmptyView.swift
@@ -0,0 +1,59 @@
+//
+//  EmptyView.swift
+//
+//
+//  Created by Taha Mahmoud on 19/01/2024.
+//
+
+import SwiftUI
+
+public struct EmptyView: View {
+    public var icon: Image?
+    public var title: String?
+    public var titleColor: Color = DesignSystem.colors.white
+    public var subTitle: String?
+    public var subTitleColor: Color = DesignSystem.colors.secondaryGray
+
+    public init(icon: Image? = nil,
+                title: String? = nil,
+                titleColor: Color = DesignSystem.colors.white,
+                subTitle: String? = nil,
+                subTitleColor: Color = DesignSystem.colors.secondaryGray) {
+        self.icon = icon
+        self.title = title
+        self.titleColor = titleColor
+        self.subTitle = subTitle
+        self.subTitleColor = subTitleColor
+    }
+
+    public var body: some View {
+        VStack(alignment: .center) {
+            if let icon = icon {
+                icon
+                    .resizable()
+                    .frame(width: 76, height: 76)
+            }
+            if let title = title, !title.isEmpty {
+                Text(title)
+                    .font(Font.montserrat(weight: .semiBold, size: 16))
+                    .foregroundStyle(titleColor)
+                    .padding(.top, 23)
+                    .multilineTextAlignment(.center)
+            }
+            if let subTitle = subTitle, !subTitle.isEmpty {
+                Text(subTitle)
+                    .foregroundStyle(subTitleColor)
+                    .padding(.top, 8)
+                    .multilineTextAlignment(.center)
+            }
+        }
+        .padding(50)
+    }
+}
+
+#Preview {
+    EmptyView(icon: Image(systemName: "plus"),
+              title: "we are sorry, we can not find any movies :(",
+              subTitle: "Find your movie by Type title, categories, years, etc ")
+    .background(DesignSystem.colors.black)
+}
---
+++ iMovies/DesignSystem/Sources/DesignSystem/Components/Product/RatingView.swift
@@ -0,0 +1,31 @@
+//
+//  RatingView.swift
+//
+//
+//  Created by Taha Mahmoud on 19/01/2024.
+//
+
+import SwiftUI
+
+public struct RatingView: View {
+    public var rating: Double
+
+    public init(rating: Double) {
+        self.rating = rating
+    }
+
+    public var body: some View {
+        HStack(alignment: .center, spacing: 4) {
+            Image(.icRatingStar)
+                .resizable()
+                .frame(width: 16, height: 16)
+            Text(rating.string)
+                .font(Font.montserrat(weight: .semiBold, size: 12))
+                .foregroundStyle(DesignSystem.colors.primary)
+        }
+    }
+}
+
+#Preview {
+    RatingView(rating: 4.9)
+}
