6734a3e20af510cd538bdebf463f95fc55c4b31f
Taha Mahmoud <taha.nagy06@gmail.com>
2024-01-19 12:55:48 +0200

    Add ShortMovie and DescriptionMovie Views
    Add ShortMovieItemView and DescriptionMovieView to use them in the app

---
+++ iMovies/DesignSystem/Sources/DesignSystem/Components/Product/DescriptionMovieItemView.swift
@@ -0,0 +1,83 @@
+//
+//  DescriptionMovieItemView.swift
+//
+//
+//  Created by Taha Mahmoud on 19/01/2024.
+//
+
+import SwiftUI
+
+public struct DescriptionMovieItemViewModel {
+    public var posterURL: String
+    public var movieName: String
+    public var rating: Double
+    public var genre: [String]
+    public var year: Int
+    public var duration: Int
+}
+
+public struct DescriptionMovieItemView: View {
+    public var viewModel: DescriptionMovieItemViewModel
+
+    public init(viewModel: DescriptionMovieItemViewModel) {
+        self.viewModel = viewModel
+    }
+
+    public var body: some View {
+        HStack(alignment: .top, spacing: 12) {
+            moviePoster
+            movieInfo
+        }
+        .padding()
+    }
+
+    var moviePoster: some View {
+        RemoteImage(url: viewModel.posterURL)
+            .frame(width: 95, height: 120)
+            .clipShape(RoundedRectangle(cornerRadius: 16))
+    }
+
+    var movieInfo: some View {
+        VStack(alignment: .leading) {
+            Text(viewModel.movieName)
+                .font(Font.montserrat(weight: .regular, size: 16))
+                .foregroundStyle(DesignSystem.colors.white)
+                .lineLimit(1)
+            VStack(alignment: .leading, spacing: 4) {
+                RatingView(rating: viewModel.rating)
+                HStack(spacing: 4) {
+                    Image(.icGenre)
+                    Text(viewModel.genre.joined(separator: ", "))
+                        .foregroundStyle(DesignSystem.colors.white)
+                        .font(Font.montserrat(weight: .regular, size: 16))
+                }
+                HStack(spacing: 4) {
+                    Image(.icDate)
+                    Text(viewModel.year.string)
+                        .foregroundStyle(DesignSystem.colors.white)
+                        .font(Font.montserrat(weight: .regular, size: 16))
+                }
+                HStack(spacing: 4) {
+                    Image(.icDuration)
+                    Text("\(viewModel.duration) minutes")
+                        .foregroundStyle(DesignSystem.colors.white)
+                        .font(Font.montserrat(weight: .regular, size: 16))
+                }
+            }
+            .padding(.top, 14)
+            .padding(.trailing, 16)
+        }
+    }
+}
+
+#Preview {
+    DescriptionMovieItemView(
+        viewModel: .init(
+            posterURL: "https://m.media-amazon.com/images/M/MV5BMjMyOTM4MDMxNV5BMl5BanBnXkFtZTcwNjIyNzExOA@@._V1_FMjpg_UX1000_.jpg",
+            movieName: "Spiderman Spiderman Spiderman",
+            rating: 9.5,
+            genre: ["Action", "Drama"],
+            year: 2019,
+            duration: 139))
+        .background(Color.black)
+}
---
+++ iMovies/DesignSystem/Sources/DesignSystem/Components/Product/ShortMovieItemView.swift
@@ -0,0 +1,70 @@
+//
+//  ShortMovieItemView.swift
+//
+//
+//  Created by Taha Mahmoud on 19/01/2024.
+//
+
+import SwiftUI
+
+public struct ShortMovieItemView: View {
+    public var imageURL: String
+    public var name: String?
+    public var rating: Double?
+
+    public init(imageURL: String,
+                name: String? = nil,
+                rating: Double? = nil) {
+        self.imageURL = imageURL
+        self.name = name
+        self.rating = rating
+    }
+
+    public var body: some View {
+        ZStack(alignment: .bottom) {
+            moviePoster
+                .frame(width: 120, height: 200)
+            linearGradient
+            movieInfo
+        }
+        .frame(width: 120, height: 200)
+    }
+
+    var moviePoster: some View {
+        RemoteImage(url: imageURL)
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+    }
+
+    var linearGradient: some View {
+        LinearGradient(
+            gradient: Gradient(
+                colors: [Color.black.opacity(0.6), Color.clear]),
+            startPoint: .bottom,
+            endPoint: .top)
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+    }
+
+    var movieInfo: some View {
+        HStack {
+            if let name = name, !name.isEmpty {
+                Text(name)
+                    .font(Font.montserrat(weight: .semiBold, size: 10))
+                    .foregroundStyle(DesignSystem.colors.white)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(2)
+            }
+            Spacer()
+            if let rating = rating, rating != 0 {
+                RatingView(rating: rating)
+            }
+        }
+        .padding(12)
+    }
+}
+
+#Preview {
+    ShortMovieItemView(imageURL: "https://m.media-amazon.com/images/M/MV5BMjMyOTM4MDMxNV5BMl5BanBnXkFtZTcwNjIyNzExOA@@._V1_FMjpg_UX1000_.jpg",
+                       name: "Spiderman",
+                       rating: 4.3)
+        .background(Color.black)
+}
